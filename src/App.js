6723b67399d1ae238094322bf4cc86b5f1d8af78
since <498800103@qq.com>
2020-12-10 09:37:44 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './App.scss'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='a'>
+        111
+      </div>
+    )
+  }
+}
